3dd436dc3e932d6af6bf2e2cfce82b162c14c441
Diego Salazar <diegosalazargm@gmail.com>
2024-02-09 17:09:08 -0400

    Addin example back again

---
+++ example/src/components/form.tsx
@@ -0,0 +1,37 @@
+import { Box, HStack, useDisclosure } from "@chakra-ui/react";
+import { useState } from "react";
+import {
+  MonthInput,
+  MonthPicker,
+  MonthPickerValue,
+} from "@diegosalazar27/chakra-month-picker/src/index";
+
+export function Form() {
+  const [value, setValue] = useState<MonthPickerValue>({
+    month: 0,
+    year: 2024,
+  });
+  const { isOpen, onClose, onToggle } = useDisclosure(); // Chakra Hook to handle open/close
+
+  function handleChange(data: MonthPickerValue) {
+    setValue(data);
+  }
+
+  return (
+    <HStack spacing={4} maxW={"container.lg"}>
+      <Box position={"relative"}>
+        <MonthInput value={value} toggle={onToggle} isOpen={isOpen} />
+        <MonthPicker
+          lang="es"
+          isOpen={isOpen}
+          value={value}
+          onChange={handleChange}
+          onClose={onClose}
+        />
+      </Box>
+      <Box maxW={"400px"}>
+        <pre>{JSON.stringify(value, null, 2)}</pre>
+      </Box>
+    </HStack>
+  );
+}
